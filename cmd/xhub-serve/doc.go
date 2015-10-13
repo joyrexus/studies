4ab1e831f23aa4b1745d9d8bb506a1d250abc644
Jason Voigt <joyrexus@gmail.com>
2015-10-13 10:08:26 -0500

    Add flags for specifying addr and dbfile

--- cmd/xhub-serve/doc.go
+++ cmd/xhub-serve/doc.go
@@ -1,2 +1,13 @@
-// xhub-serve is a command for starting the xhub server.
+/* 
+xhub-serve is a command for starting the xhub server.
+
+Usage:
+	xhub-serve [flags]
+
+The flags are:
+	-addr
+		host name or ip address (`localhost:8081`)
+	-dbfile
+		name of the boltdb file for persisting xhub data (`xhub.db`)
+*/
 package main
--- cmd/xhub-serve/main.go
+++ cmd/xhub-serve/main.go
@@ -1,20 +1,22 @@
 package main
 
 import (
-	"fmt"
+	"flag"
 	"log"
 
 	"github.com/joyrexus/xhub"
 )
 
-const (
-	host   = "localhost"  // host name or ip address
-	port   = 8081         // host port number
-	dbfile = "xhub.db" // path to file to use for persisting xhub data
+var (
+	addr   string
+	dbfile string
 )
 
 func main() {
-	addr := fmt.Sprintf("%s:%d", host, port)
+	flag.StringVar(&addr, "addr", "localhost:8081", "host name or ip address")
+	flag.StringVar(&dbfile, "dbfile", "xhub.db", "path to database file")
+	flag.Parse()
+
 	srv := xhub.NewServer(addr, dbfile)
 	log.Fatal(srv.ListenAndServe())
 }
